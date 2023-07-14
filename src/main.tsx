b188daec121cd7f329835e565835a00e1088869c
VictorMFL <vm859222@gmail.com>
2023-07-14 18:39:48 -0300

    configurando styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+// Context
 import { Providers } from "./providers/Provaiders";
+
+// Config do Styled-components
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Providers>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Providers>
 );
---
+++ src/styles/theme.tsx
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    white: "#fff",
+    black: "#000"
+  }
+}
+
+export default theme
